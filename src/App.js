8104dcbc29961b4858832ca9606d54afa7b3795a
Hanna Kim <hannakim91@gmail.com>
2020-11-28 14:36:01 -0700

    Add state to App and add basic counter and log in elem to DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <h2>Counter {counter}</h2>
+      <button>+</button>
+      <button>-</button>
+      {isLogged ? <h3>Display if logged in</h3> : ''}
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer
+});
+
+export default rootReducer;
+
+// es6 syntactic sugar
+// combineReducers({
+//   counterReducer,
+//   loggedReducer
+// });
